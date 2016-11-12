4125b6b1cc9ee512d82c9c086e6f36734ce1fd3e
Horia Radu <horia.radu23@gmail.com>
2016-11-12 14:31:15 +0200

    module pattern

--- app/src/app.js
+++ app/src/app.js
@@ -1,74 +1,78 @@
-const HANGMAN = {
-  state: {
+const HANGMAN = (() => {
+  const state = {
     word: null,
     inputWord: null,
     tries: new Set(),
     won: false
-  },
+  };
 
-  getRandom(max){
+  return {
+    init
+  };
+
+  function getRandom(max) {
     return Math.floor(Math.random() * max);
-  },
+  }
 
   /** Game logic **/
 
-  init(words) {
-    this.state.word = words[this.getRandom(words.length)];
-    this.state.tries = new Set();
-    this.state.inputWord = this.emptyWord(this.state.word);
-    this.state.won = false;
-    console.log(this.state.word);
+  function init(words) {
+    state.word = words[getRandom(words.length)];
+    state.tries = new Set();
+    state.inputWord = emptyWord(state.word);
+    state.won = false;
+    console.log(state.word);
 
-    this.initView(this.state);
-  },
+    initView(state);
+  }
 
-  initView(state) {
+  function initView(state) {
     const wrapper = $('#app');
     wrapper.append(`<div id="title">Hangman</div>`);
     wrapper.append(`<ul></ul>`);
 
-    this.draw(wrapper, state.inputWord);
+    draw(wrapper, state.inputWord);
 
     $('body').on('keypress', (event) => {
       const key = event.key || String.fromCharCode(event.keyCode);
 
-      this.validate(key, state.inputWord, state.word);
+      validate(key, state.inputWord, state.word);
 
-      this.draw(wrapper, state.inputWord, state.won);
+      draw(wrapper, state.inputWord, state.won);
     });
-  },
+  }
 
-  emptyWord(word) {
+  function emptyWord(word) {
     return new Array(word.length).fill('_');
-  },
+  }
 
-  validate(letter, word, solution) {
-    if (this.state.tries.has(letter)) {
+  function validate(letter, word, solution) {
+    if (state.tries.has(letter)) {
       console.log('already added that');
     } else if (solution.includes(letter)) {
-      this.addLetter(letter, word, solution);
+      addLetter(letter, word, solution);
     } else {
       console.log('wrong!');
     }
 
-    this.state.tries.add(letter);
+    state.tries.add(letter);
 
     if (!word.includes('_')) {
-      this.state.won = true;
+      state.won = true;
       console.log('win!');
     }
-  },
+  }
 
-  addLetter(letter, word, solution) {
+  function addLetter(letter, word, solution) {
     for (let i = 0; i < word.length; i += 1) {
       if (solution[i] == letter) {
         word[i] = letter;
       }
     }
-  },
+  }
 
   /** UI stuff **/
-  draw(wrapper, word, won) {
+  function draw(wrapper, word, won) {
     const charList = word.map((char) => `<li>${char}</li>`);
     wrapper.find('ul').html(charList);
 
@@ -76,4 +80,4 @@ const HANGMAN = {
       wrapper.find('#title').html('You won!');
     }
   }
-};
+})();
